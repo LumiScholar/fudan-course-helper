63e8df69bb86eb13d2a56d8a70b4a2da80c82466
Natica-308 <543323391@qq.com>
2026-09-16 20:52:49 +0800

    Clean popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-/usr/local/Homebrew/Library/Homebrew/cmd/shellenv.sh: line 27: /bin/ps: Operation not permitted
 const timeInput = document.querySelector('#refreshTime');
 const startButton = document.querySelector('#start');
 const cancelButton = document.querySelector('#cancel');
